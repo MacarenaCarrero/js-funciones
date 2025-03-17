8cdb496254a52d912c7dc9e4001a291280f9d226
Marialafu <macarena.c.rios@gmail.com>
2025-03-17 11:37:57 +0100

    js-funciones

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+//las variables fuera de una función son globales y las variables que se declaran dentros de una función, solo se aplica a esa función y no la podrás llamar fuera de ella.
+let numberA = 89;
+
+const numberB = 64;
+
+console.log(numberA + numberB);
+
+numberA = 12;
+console.log(numberB + numberA);
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+//funcion
+function printOne() {
+  console.log(numberA);
+  console.log(numberB);
+}
+
+// llamar a la función. (Las veces que la necesite)
+printOne();
+
+// function printFour() {
+//   console.log(4);
+// }
+// function printFive() {
+//   console.log(5);
+// }
+// function printSix() {
+//   console.log(6);
+// }
+
+// printFour();
+// printFive();
+// printSix();
+
+//en vez de hacer lo de arroba podemos hacer la siguiente función
+
+function printNumber(number) {
+  console.log(number);
+}
+
+printNumber(1);
+printNumber(2);
+printNumber(3);
+
+//se puede poner separados por comas
+function printOperation(numberC, numberD) {
+  console.log(numberC + numberD);
+}
+
+printOperation(2, 3);
